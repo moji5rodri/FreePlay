94d85dc039679333f8bcf908cfe28490f9fc74cd
a19j601 <rodrigocarrascgonzales>
2018-04-24 11:05:30 -0500

    Initial commit

---
+++ app/src/main/java/com/codex/freeplay/MatchesActivity.kt
@@ -0,0 +1,12 @@
+package com.codex.freeplay
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_matches)
+    }
+}
